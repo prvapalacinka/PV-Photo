85880eb9d199f09bb3a1f46fb8aa11a98888da9e
Broly <vidovicp20@gmail.com>
2022-12-15 12:01:39 -0500

    created backend for user

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connections.js');
+const {User} = require('./models/index');
+const app = express();
+
+
+const PORT = 3001;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/user', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+    })
+    .then((newUser) => {
+        res.json(newUser);
+    })
+})
+db.once('open', () => {
+    app.listen(PORT);
+});
